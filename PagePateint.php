c3f21443159805f737b222b519c70cd1c592b87b
MeyWast <51417462+MeyWast@users.noreply.github.com>
2022-04-08 10:42:56 +0200

    Create PagePateint.php

---
+++ PagePateint.php
@@ -0,0 +1,14 @@
+<?php
+  // Initialiser la session
+  session_start();
+  // Vérifiez si l'utilisateur est connecté, sinon redirigez-le vers la page de connexion
+  if(!isset($_SESSION('email'])){
+    header("Location : authentification.php");
+    exit();
+  }
+?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    
