8f218ebd3a5b33fb13af702f23ddecb9c1924577
MeyWast <51417462+MeyWast@users.noreply.github.com>
2022-04-08 23:43:25 +0200

    Create rendezvous.sql

---
+++ rendezvous.sql
@@ -0,0 +1,18 @@
+CREATE TABLE Patient (
+  nom VARCHAR(50) NOT NULL,
+  prenom VARCHAR(50) NOT NULL,
+  email VARCHAR(50) NOT NULL,
+  mdp text NOT NULL,
+  PRIMARY KEY(email);
+);
+
+CREATE TABLE Medecin (
+  nom VARCHAR(50) NOT NULL,
+  prenom VARCHAR(50) NOT NULL,
+  telephone VARCHAR(50) NOT NULL,
+  email VARCHAR(50) NOT NULL,
+  mdp text NOT NULL,
+  specialite VARCHAR(50)
+  nombre_mdp int(1) DEFAULT 0;
+  PRIMARY KEY(email);
+ );
