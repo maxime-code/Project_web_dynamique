152353944d568d08bdda5198122ebf6b96ec43fa
MeyWast <51417462+MeyWast@users.noreply.github.com>
2022-04-09 00:23:17 +0200

    Update deconnexion.php

--- deconnexion.php
+++ deconnexion.php
@@ -6,6 +6,6 @@
   if(session_destroy())
   {
     // Redirection vers la page de connexion
-    header("Location: authentification.php");
+    header("Location: authentificationPatient.php");
   }
 ?>
