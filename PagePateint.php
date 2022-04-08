a3889b514763b28b765f0205f2946f81ad58925f
MeyWast <51417462+MeyWast@users.noreply.github.com>
2022-04-08 11:34:34 +0200

    Update PagePateint.php

--- PagePateint.php
+++ PagePateint.php
@@ -11,4 +11,12 @@
 <html>
   <head>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    
+  </head>
+  <body>
+    <div>
+      <h1> Bienvenue <?php echo $_SESSION['email']; ?> ! </h1>
+      <a href = "deconnexion.php"> Déconnexion </a>
+    </div>
+  </body>
+</html>
+
