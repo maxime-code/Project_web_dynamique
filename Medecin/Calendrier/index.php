2db123c7a363cdf3f656fe487e16514dfc46052a
MeyWast <51417462+MeyWast@users.noreply.github.com>
2022-04-10 03:46:05 +0200

    Update index.php

--- Medecin/Calendrier/index.php
+++ Medecin/Calendrier/index.php
@@ -13,21 +13,37 @@
   <?php
     require '/Date/Mois.php';
     $mois = new Mois(mois: $_GET['mois'] ?? null . $_GET['an'] ?? null);
+		$jour = $mois->getPremierJour()->modify(modify:'last monday');
   ?>
   
-  <h1> <?php echo $mois->StringMois(); ?> </h1>
+	<div class="d-flex flex-row align-items-center justify-content-between mx-sm-3">
+		<h1><?php echo $mois->StringMois(); ?></h1>
+		<div>
+			<a href="index.php?month=<?php echo $mois->DernierMois()->mois;?>&an=<?php echo $mois->DernierMois()->an" class="btn btn-primary">&lt</a>
+			<a href="index.php?month=<?php echo $mois->ProchainMois()->mois;?>&an=<?php echo $mois->ProchainMois()->an" class="btn btn-primary">&gt</a>
+		</div>			
+	</div>
+  <h1 class="h-10"> <?php echo $mois->StringMois(); ?> </h1>
   <?php echo $mois->getSemaine(); ?>
-  <table class="w-100 p-3" >
+	// faut faire le css et tt
+  <table class="w-100 h-50">
     <?php for ($i =0; $i < $mois->getSemaine(); $i++); ?>
     <tr>
-      <td> Lundi </td>
-      <td> Mardi </td>
-      <td> Mercredi </td>
-      <td> Jeudi </td>
-      <td> Vendredi </td>
-      <td> Samedi </td>
-      <td> Dimanche </td>
-    </tr>
+<!-- 	<td class="p-10 border border-secondary w-14.29 h-20"> Lundi <br> <?php echo $jour->format(format:'d'); ?> </td>
+      <td class="p-10 border border-secondary w-14.29 h-20"> Mardi <br> <?php echo $jour->format(format:'d'); ?> </td>
+      <td class="p-10 border border-secondary w-14.29 h-20"> Mercredi <br> <?php echo $jour->format(format:'d'); ?> </td>
+      <td class="p-10 border border-secondary w-14.29 h-20"> Jeudi <br> <?php echo $jour->format(format:'d'); ?> </td>
+      <td class="p-10 border border-secondary w-14.29 h-20"> Vendredi <br> <?php echo $jour->format(format:'d'); ?> </td>
+      <td class="p-10 border border-secondary w-14.29 h-20"> Samedi <br> <?php echo $jour->format(format:'d'); ?> </td>
+      <td class="p-10 border border-secondary w-14.29 h-20"> Dimanche <br> <?php echo $jour->format(format:'d'); ?> </td> -->
+    	<?php foreach($mois->days as $k => $day) : ?>
+			<td>
+				<?php if($i === 0) : ?>
+				<div class="font-weight-bold"> <?php echo $day; ?> </div>
+				<?php endif; ?>
+				<div> <?php echo (clone $jour)->modify(modify:"+".{$k + $i *7} ."days")->format(format:'d'); ?> </div>
+			</tr>
+		<?php endfor; ?>
   </table>
     
 <body>
