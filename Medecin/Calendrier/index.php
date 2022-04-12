f19d2d6bbb755602ea1f522fc980894dce39a156
MeyWast <51417462+MeyWast@users.noreply.github.com>
2022-04-12 23:12:50 +0200

    Update index.php

--- Medecin/Calendrier/index.php
+++ Medecin/Calendrier/index.php
@@ -11,11 +11,17 @@
   </nav>
   
   <?php
+    require '/Date/Evenements.php';
     require '/Date/Mois.php';
-	$debut = $mois->getPremierJour();
+	$evenement = new Evenements();
     $mois = new Mois(mois: $_GET['mois'] ?? null . $_GET['an'] ?? null);
+	$debut = $mois->getPremierJour();
 		$debut = $debut->format(format:'N') === '1' ? $debut : $mois->getPremierJour()->modify(modify:'last monday');
-  ?>
+  	$semain = $mois->getSemaine();
+	$fin = (clone $debut)->modify(modify:'+' .(6 + 7* ($semaine - 1)) . 'days');
+	
+	$evenement = $evenement->getEvenementsEntreParJour($debut, $fin);
+	?>
   
 	<div class="d-flex flex-row align-items-center justify-content-between mx-sm-3">
 		<h1><?php echo $mois->StringMois(); ?></h1>
@@ -30,14 +36,21 @@
   <table class="w-100 h-50">
     <?php for ($i =0; $i < $mois->getSemaine(); $i++); ?>
     <tr>
-    	<?php foreach($mois->days as $k => $day) : ?>
+    	<?php foreach($mois->days as $k => $day) :
+		$date = (clone $jour)->modify(modify:"+".{$k + $i *7} ."days");
+		$events = $evenement[$date->format(format: 'Y-m-d')] ?? [];
+	?>
 			<td>
 				<?php if($i === 0) : ?>
 				<div class="font-weight-bold"> <?php echo $day; ?> </div>
 				<?php endif; ?>
-				<div> <?php echo (clone $jour)->modify(modify:"+".{$k + $i *7} ."days")->format(format:'d'); ?> </div>
-			</tr>
+				<div> <?php echo $date->format(format:'d'); ?> </div>
+				<?php foreach($events as $event) : ?>
+					<div> <?php echo {new DateTime($event['debut'])}->format(format:'H:i') ?> - <a href="/events.php?id=<?php echo $event['id']; ?> " ><?php echo $event['name']; ?></a> </div>
+				<?php endforeach; ?>
+			</td>
 		<?php endfor; ?>
+	</tr>
   </table>
     
 <body>
