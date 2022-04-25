5bc7c517a3ccf64851882db7fcbb351ca5f3d4b8
MeyWast <51417462+MeyWast@users.noreply.github.com>
2022-04-25 17:31:54 +0200

    Update PagePatient.php

--- Patient/PagePatient.php
+++ Patient/PagePatient.php
@@ -2,7 +2,7 @@
   // Initialiser la session
   session_start();
   // Vérifiez si l'utilisateur est connecté, sinon redirigez-le vers la page de connexion
-  if(!isset($_SESSION('email'])){
+  if(!isset($_SESSION['email'])){
     header("Location : authentification.php");
     exit();
  }
@@ -17,13 +17,16 @@
       <h1> Bienvenue <?php echo $_SESSION['email']; ?> ! </h1>
   
         <h1> Prendre un Rendez Vous </h1>
+      <form action="" method="post">
         <input type="text" class="form-control" name="medecin" placeholder="Nom du médecin" />
         <input type="text" class="form-control" name="specialite" placeholder "Spécialité" />
         <input type="date" class="form-control" name="date" placeholder="Date" />
         <input type="text" class="form-control" name="codepostal" placeholder="Code Postal" />
         <input type="submit" name="submit" value="Rechercher" class="btn btn-secondary" />
+      </form>
   <?php
      $db = dbConnect();
+     if(isset($_POST['submit']) {
      if(!isset($_POST['medecin'],$_POST['specialite'], $_POST['date'], $_POST['codepostal'])){
        echo "Veuillez au moins remplir un de ces critères"
        } else {
@@ -33,8 +36,11 @@
          $statement->execute();
          $result = $statement->fetchAll(PDO::FETCH_ASSOC);
          foreach($result){
-           echo "Monsieur ".$result['nom']
-         
+           echo "<form action='/Medecin.php' method='get'>";
+           echo "Docteur ".$result['nom']." ".$result['prenom']." situé en ".$result['codepostal'].", spécialiste en ".$result['specialite'].";
+           echo "<a href='/Medecin.php?email='.$result['email']."'> En Savoir Plus </a>"
+           }
+       }
        
       <a href = "deconnexion.php"> Déconnexion </a>
     </div>
