6b8876fb1c9a7c24c964e5b3f73d62a80c635e72
MeyWast <51417462+MeyWast@users.noreply.github.com>
2022-04-30 22:56:29 +0200

    Update authentificationPatient.php

--- authentificationPatient.php
+++ authentificationPatient.php
@@ -1,3 +1,12 @@
+<?php
+include ("database.php");
+
+ini_set('display_errors',1);
+error_reporting(E_ALL);
+dbConnect();
+$db = dbConnect();
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -5,15 +14,39 @@
 </head>
 <body>
 
+<?php
+  session_start();
+  if(isset($_POST['submit']) && !empty($_POST['submit'])){
+    $email = htmlspecialchars($_POST['email']);
+    $mdp = htmlspecialchars($_POST['password']);
+    $request = 'SELECT email, mdp FROM patient WHERE email=:email AND mdp=:mdp';
+    $statement = $db->prepare($request);
+    $statement->bindParam(':email', $email);
+    $statement->bindParam(':mdp',$mdp);
+    $statement->execute();
+    $result = $statement->fetch();
+    $row = $statement->rowCount();
+    if($row == 1)
+    {
+      $_SESSION['email'] = $result['email'];
+      header("Location: patient.php");
+    }else{
+        $erreur = "Vos identifiants sont incorrect.";
+    }  
+}
+ ?>
+
 <form action="" method="post">
 <h1>Se connecter</h1>
 <input type="text" class="form-control" name="email" placeholder="Email" required />
 <input type="password" class="form-control" name="password" placeholder="Mot de passe" required />
 <input type="submit" name="submit" value="Se connecter" class="btn btn-secondary" />
 </form>
-<form action="/inscriptionPatient.php" method="post">
-    <p> Pas encore inscrit ?
-    <button type="submit">Faites-le ici</button>
+<form action="inscriptionPatient.php" metho d="post">
+    <p> Pas encore inscrit ?  <button type="submit">Faites-le ici</button> <p>
+    <?php if (! empty($erreur)) { ?>
+  <p><?php echo $erreur; ?></p>
+<?php } ?>
 </form>
 </body>
 </html>
