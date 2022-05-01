49b35a22554c219ed43b02698138faea25aeff4b
MeyWast <51417462+MeyWast@users.noreply.github.com>
2022-05-01 21:51:25 +0200

    Update inscriptionPatient.php

--- inscriptionPatient.php
+++ inscriptionPatient.php
@@ -13,7 +13,7 @@ $db = dbConnect();
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 </head>
 <body>
-  
+  <div class="login-form">
 <?php
 
 if (isset($_POST['submit']) && !empty($_POST['submit'])){
@@ -45,17 +45,54 @@ if (isset($_POST['submit']) && !empty($_POST['submit'])){
 }else{
 ?>
 <form action="" method="post">
-<h1>S'inscrire</h1>
+<h1 class="text-center">S'inscrire</h1>
+  <div class="form-group">
 <input type="text" class="form-control" name="nom" placeholder="Nom" required />
+  </div>
+  <div class="form-group">
 <input type="text" class="form-control" name="prenom" placeholder="Prénom" required />
+  </div>
+  <div class="form-group">
 <input type="number" class="form-control" name="phone" placeholder="Téléphone" required />
+  </div>
+  <div class="form-group">
 <input type="email" class="form-control" name="email" placeholder="Email" required />
+  </div>
+  <div class="form-group">
 <input type="email" class="form-control" name="email2" placeholder="Confirmation de votre email" required />
+  </div>
+  <div class="form-group">
 <input type="password" class="form-control" name="password" placeholder="Mot de passe" required />
+  </div>
+  <div class="form-group">
 <input type="submit" name="submit" value="S'inscrire" class="btn btn-secondary" />
+  </div>
 </form>
-<form action="authentificationPatient.php" method="post">
-    <p> Déjà inscrit ?  <button type="submit">Connectez vous ici</button> <p>
+  </div>
+<p class="text-center"><a href="authentificationPatient.php"> Vous avez déjà un compte </a></p>
 <?php } ?>
-</body>
+<style>
+            .login-form {
+                width: 340px;
+                margin: 50px auto;
+            }
+            .login-form form {
+                margin-bottom: 15px;
+                background: #f7f7f7;
+                box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.3);
+                padding: 30px;
+            }
+            .login-form h2 {
+                margin: 0 0 15px;
+            }
+            .form-control, .btn {
+                min-height: 38px;
+                border-radius: 2px;
+            }
+            .btn {        
+                font-size: 15px;
+                font-weight: bold;
+            }
+        </style>
+  </body>
 </html>
