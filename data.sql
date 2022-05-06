88df9962a84813c4a7b1227bca860bb8d2f99440
maxime-code <71320650+maxime-code@users.noreply.github.com>
2022-05-06 16:00:26 +0200

    Update data.sql

--- data.sql
+++ data.sql
@@ -1,4 +1,3 @@
-
 DELETE FROM medecin;
 DELETE FROM patient;
 DELETE FROM rdv;
@@ -11,8 +10,10 @@ INSERT INTO medecin (email, nom, prenom, mdp, telephone, codepostal, specialite)
 ('ziox.fake@gmail.com', 'Joestar', 'Joseph', 'jojo','0767424852', '44980', 'onde'),
 ('alexandre.auguste44@gmail.com', 'Zeppli', 'Ceasar', 'savon', '0782083299', '44700', 'mort');
 
---INSERT INTO rdv (debut, fin, libre, medecinemail, patientemail) VALUES
---(mktime(0,0,0,01,01,2008), mktime(0,0,0,01,01,2008), TRUE,'ziox.fake@gmail.com','alexandre.auguste@isen-ouest.yncrea.fr' ),
---(1627650000, 1627653600,TRUE,'alexandre.auguste44@gmail.com','alexandre.auguste44@gmail.com');
+INSERT INTO rdv (debut, fin, libre, medecinemail, patientemail, informations) VALUES
+('2016-06-22 19:10:25-07', '2016-06-22 20:10:25-07', TRUE,'ziox.fake@gmail.com','alexandre.auguste@isen-ouest.yncrea.fr', 'Je suis un medecin'),
+('2016-06-22 12:10:25-07','2016-06-22 13:10:25-07',TRUE, 'alexandre.auguste44@gmail.com','alexandre.auguste44@gmail.com', 'Je suis un bon medecin');
+
+
 
 
