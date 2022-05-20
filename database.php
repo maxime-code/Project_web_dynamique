8aad49e2b6b6b47db8ee2fb507592ab32904b03f
MeyWast <51417462+MeyWast@users.noreply.github.com>
2022-05-20 11:09:43 +0200

    Update database.php

--- database.php
+++ database.php
@@ -24,20 +24,33 @@ function dbConnect()
     }
 }
 
-function verifrdv($medecin, $date, $text, $db)
+function verifrdvpatient($db,$timestamp,$medecin,$heure)
 {
-    $request = 'SELECT * FROM rdv WHERE medecinemail=:medecin AND debut=:debut';
+    $request = 'SELECT * FROM rdv WHERE heure=:heure AND medecinemail=:medecinemail';
     $statement = $db->prepare($request);
-    $statement->bindParam(':medecin',$medecin);
-    $statement->bindParam(':debut',$date);
+    $statement->bindParam(":heure",$timestamp);
+    $statement->bindParam(":medecinemail",$medecin);
     $statement->execute();
-    $result = $statement->fetch(PDO::FETCH_ASSOC);
-
-    if($statement->rowCount()==1){
-        return "-----";
-    } else {
-        return "<button type='submit' class='btn btn-secondary' name='heure' value='".$date."'>".$text." </button>";
+    $row = $statement->rowCount();
+    $result = $statement->fetchAll(PDO::FETCH_ASSOC);
+    if($row == 0)
+    {
+        echo "<td style='text-align: center'> <button type='submit' class='btn btn-secondary' name='heure' value='".$timestamp."'> ".$heure." </button> </td>";
     }
+    else{
+      echo "<td style='text-align: center'> <button class='btn btn-danger' disabled> --------- </button> </td> ";
+    }
+}
+
+function verifrdvmedecin($db,$timestamp,$medecin)
+{
+    $request = 'SELECT * FROM rdv WHERE heure=:heure AND medecinemail=:medecinemail';
+    $statement = $db->prepare($request);
+    $statement->bindParam(":heure",$timestamp);
+    $statement->bindParam(":medecinemail",$medecin);
+    $statement->execute();
+    $row = $statement->rowCount();
+    return $result = $statement->fetchAll(PDO::FETCH_ASSOC);
 }
 
 function ajouterDay($i,$plus,$moins)
@@ -45,6 +58,14 @@ function ajouterDay($i,$plus,$moins)
     setlocale(LC_TIME, "fr_FR");
     $Jour = strftime("%A - %d/%m/%Y", strtotime("this week +".$i." day -".$moins." week +".$plus." week"));
     return $Jour;
+}
+
+function getActualTimeStamp()
+{
+    setlocale(LC_TIME, "fr_FR");
+    $heure = strftime("%Y-%m-%d H:i:s", strtotime("this week"));
+    $heure = $heure."-00";
+    return $heure;
 }
 
 function getTimeStamp($plus,$plusweek,$moinsweek)
@@ -60,5 +81,6 @@ function addHour($TimeStamp)
     $heure =    date('Y-m-d H:i:s',strtotime('-1 hour',strtotime($TimeStamp)));
     return $heure;
 }
+
 
 ?>
